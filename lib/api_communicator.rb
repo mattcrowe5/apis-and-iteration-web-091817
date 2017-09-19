fce462850e14da7d2e8cd33832f29ff907f1c865
mattcrowe5 <crowe.matthew5@gmail.com>
2017-09-19 13:42:05 -0400

    added code worked on with Ramy

--- lib/api_communicator.rb
+++ lib/api_communicator.rb
@@ -4,23 +4,21 @@ require 'pry'
 
 def get_character_movies_from_api(character)
   #make the web request
-  all_characters = RestClient.get('http://www.swapi.co/api/people/')
-  character_hash = JSON.parse(all_characters)
-  
-  # iterate over the character hash to find the collection of `films` for the given
-  #   `character`
-  # collect those film API urls, make a web request to each URL to get the info
-  #  for that film
-  # return value of this method should be collection of info about each film.
-  #  i.e. an array of hashes in which each hash reps a given film
-  # this collection will be the argument given to `parse_character_movies`
-  #  and that method will do some nice presentation stuff: puts out a list
-  #  of movies by title. play around with puts out other info about a given film.
+  movie_links = character_movies(character)
+  movie_info(movie_links)
 end
 
+
+
 def parse_character_movies(films_hash)
+  count = 0
+  films_hash.each do |movie|
+    count += 1
+    puts "#{count} #{movie["title"]}"
+  end
   # some iteration magic and puts out the movies in a nice list
 end
+
 
 def show_character_movies(character)
   films_hash = get_character_movies_from_api(character)
@@ -31,3 +29,26 @@ end
 
 # that `get_character_movies_from_api` method is probably pretty long. Does it do more than one job?
 # can you split it up into helper methods?
+
+def character_movies(character)
+  output = []
+  all_characters = RestClient.get('http://www.swapi.co/api/people/')
+  character_hash = JSON.parse(all_characters)
+  results = character_hash["results"]
+  results.each do |person|
+    if person["name"].downcase == character
+      output = person["films"]
+    end
+  end
+  return output
+end
+
+def movie_info(output)
+  filmArray = []
+  output.each do |link|
+    moviedata = RestClient.get(link)
+    moviedata_hash = JSON.parse(moviedata)
+    filmArray << moviedata_hash
+  end
+  return filmArray
+end
